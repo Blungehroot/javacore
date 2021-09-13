f2db1199b69639cc5da375b2dcc62b59ff4cfb1f
Yevgeniy Shaydur <yevgeniy.shaydur@kyivstar.net>
2021-09-13 18:15:07 +0300

    implement test thread task

--- src/main/java/com/blungehroot/javacore/multithreads/Foo.java
+++ src/main/java/com/blungehroot/javacore/multithreads/Foo.java
@@ -3,14 +3,17 @@ package com.blungehroot.javacore.multithreads;
 public class Foo {
 
     public void first() {
+        System.out.println(Thread.currentThread().getName());
         System.out.print("first");
     }
 
     public void second() {
+        System.out.println(Thread.currentThread().getName());
         System.out.print("second");
     }
 
     public void third() {
+        System.out.println(Thread.currentThread().getName());
         System.out.print("third");
     }
 
