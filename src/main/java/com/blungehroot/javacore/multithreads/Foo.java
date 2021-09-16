344ae8f9d8020299c59bd638e464295b042b394d
Yevgeniy Shaydur <yevgeniy.shaydur@kyivstar.net>
2021-09-16 17:16:44 +0300

    implement test thread task

--- src/main/java/com/blungehroot/javacore/multithreads/Foo.java
+++ src/main/java/com/blungehroot/javacore/multithreads/Foo.java
@@ -1,17 +1,59 @@
 package com.blungehroot.javacore.multithreads;
 
+import java.util.concurrent.Semaphore;
+
+import static java.lang.Thread.sleep;
+
 public class Foo {
+    private boolean access = false;
+    Semaphore semaphore = new Semaphore(1);
+    Semaphore semaphore2 = new Semaphore(1);
 
-    public void first() {
-        System.out.print("first");
+
+    public void first(Runnable r) {
+        try {
+                semaphore.acquire();
+                sleep(300);
+                System.out.print("first");
+
+                sleep(300);
+                access = true;
+                semaphore.release();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void second() {
-        System.out.print("second");
+    public void second(Runnable r) {
+        try {
+
+            semaphore2.acquire();
+            sleep(300);
+            System.out.print("second");
+
+            sleep(300);
+            semaphore2.release();
+
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void third() {
-        System.out.print("third");
+    public void third(Runnable r) {
+        try {
+
+                semaphore2.acquire();
+                sleep(300);
+                System.out.print("third");
+
+                sleep(300);
+                semaphore2.release();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- src/main/java/com/blungehroot/javacore/multithreads/Main.java
+++ src/main/java/com/blungehroot/javacore/multithreads/Main.java
@@ -1,47 +1,24 @@
 package com.blungehroot.javacore.multithreads;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutionException;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        for (int i = 0; i < 10; i++) {
-            ThreadTest threadTest = new ThreadTest();
+        Foo foo = new Foo();
+        Runnable r = () -> {
 
-            threadTest.getCf1().join();
-            if (threadTest.getCf1().isDone()) {
-                threadTest.getCf2().join();
-                if (threadTest.getCf2().isDone()) {
-                    threadTest.getCf3().join();
-                    if (threadTest.getCf3().isDone()){
-                        System.out.println();
-                    }
-                }
-            }
+        };
+        CompletableFuture<Void> cf1 = CompletableFuture.runAsync(() -> foo.second(r));
+
+        CompletableFuture.runAsync(() -> foo.third(r));
+
+        CompletableFuture.runAsync(() -> foo.first(r));
+
+        try {
+            cf1.get();
+            Thread.sleep(1000);
+        } catch (InterruptedException | ExecutionException e) {
         }
-
-        System.out.println("Main thread is finished");
-    }
-}
-
-class ThreadTest {
-    Foo foo = new Foo();
-
-    CompletableFuture<Void> cf1 = CompletableFuture.runAsync(() -> foo.first());
-
-    CompletableFuture<Void> cf2 = CompletableFuture.runAsync(() -> foo.second());
-
-    CompletableFuture<Void> cf3 = CompletableFuture.runAsync(() -> foo.third());
-
-    public CompletableFuture<Void> getCf1() {
-        return cf1;
-    }
-
-    public CompletableFuture<Void> getCf2() {
-        return cf2;
-    }
-
-    public CompletableFuture<Void> getCf3() {
-        return cf3;
     }
 }
