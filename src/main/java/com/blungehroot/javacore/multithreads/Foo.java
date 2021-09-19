349630cd48948b09358b6d0ef0ac53f09d1009a9
insight <insight2pizzza>
2021-09-19 17:45:32 +0300

    test

--- src/main/java/com/blungehroot/javacore/multithreads/Foo.java
+++ src/main/java/com/blungehroot/javacore/multithreads/Foo.java
@@ -5,43 +5,38 @@ import java.util.concurrent.Semaphore;
 import static java.lang.Thread.sleep;
 
 public class Foo {
-    Semaphore semaphore = new Semaphore(1);
+    Semaphore semaphore = new Semaphore(0);
     Semaphore semaphore2 = new Semaphore(1);
 
     public void first(Runnable r) {
         try {
-            semaphore.acquire(1);
+            semaphore2.acquire();
             System.out.print("first");
+            semaphore2.release();
             semaphore.release();
-            semaphore.drainPermits();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
     public void second(Runnable r) {
-        while (semaphore.availablePermits() != 0) {
             try {
-                semaphore2.acquire();
+                semaphore.acquire(1);
                 System.out.print("second");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        }
-        semaphore2.release();
+        semaphore.release(2);
     }
 
     public void third(Runnable r) {
-        while (semaphore.availablePermits() != 0) {
             try {
-                semaphore2.acquire();
+                semaphore.acquire(2);
                 System.out.print("third");
 
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        }
-
         semaphore2.release();
     }
 
